4dce266fa294a99d48fd60fdd58225847ed2a7e2
Bruno Vieira <bruno.vieira@monitoratec.com.br>
2021-03-01 14:30:40 -0300

    Add lesson 3 - the number trivia model with fixtures

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../../domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(
+          text: text,
+          number: number,
+        );
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      // The 'num' type can be both a 'double' and an 'int'
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+}
