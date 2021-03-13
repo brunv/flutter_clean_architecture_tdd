594f0b3fd1bac8db2229a7b497624c766565fc4f
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2021-03-13 19:23:15 -0300

    fix injection comments

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,9 @@
 // We have all of the individual pieces of the app architecture in place. Before
-// we can utilize them by building a UI through, we have to connect them
+// we can utilize them by building a UI though, we have to connect them
 // together. Since every class is decoupled from its dependencies by accepting
-// them through the constructor, we shomehow have to pass them in.
+// them through the constructor, we somehow have to pass them in.
 
-// We've been doing this all along in test with the mocked classes. Now,
+// We've been doing this all along in tests with the mocked classes. Now,
 // however, comes the time to pass in real production classes using a SERVICE
 // LOCATOR.
 
@@ -42,12 +42,12 @@ final sl = GetIt.instance;
 // singleton instace of 'GetIt' store inside 'sl'.
 
 Future<void> init() async {
-  //! Features - Number Trivia
+  //* Features - Number Trivia
 
   // Bloc
   sl.registerFactory(
     () => NumberTriviaBloc(
-      concrete: sl(),
+      concrete: sl.call(),
       random: sl(),
       inputConverter: sl(),
     ),
@@ -74,38 +74,44 @@ Future<void> init() async {
     () => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()),
   );
 
-  //! Core
+  //* Core
   sl.registerLazySingleton(() => InputConverter());
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 
-  //! External
+  //* External
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharedPreferences);
   sl.registerLazySingleton(() => http.Client());
   sl.registerLazySingleton(() => DataConnectionChecker());
 }
 
-// REGISTERING A FACTORY:
+// *REGISTERING A FACTORY:
 // The registration process is very straightforward.  Just instantiate the class
 // as usual and pass in sl() into every constructor parameter.
-//
-// Presentation logic holders such as Bloc shouldn't be registered as
-// singletons. They are very close to the UI and if your app has multiple pages
-// between which you navigate, you probably want to do some cleanup (like
-// closing Streams of a Bloc) from the dispose() method of a StatefulWidget.
 //
 // Using type inference​, the call to sl() will determine which object it should
 // pass as the given constructor argument. Of course, this is only possible when
 // the type in question is also registered.
+//
+// Presentation logic holders (or classes requiring cleanup) such as Bloc
+// shouldn't be registered as singletons. They are very close to the UI and if
+// your app has multiple pages between which you navigate, you will probably
+// gonna do some cleanup (like closing Streams of a Bloc) from the dispose()
+// method of a StatefulWidget.
+//
+// Having a singleton for classes with this kind of a disposal would lead to
+// trying to use a presentation logic holder (such as Bloc) with closed Streams,
+// instead of creating a new instance with opened Streams whenever you'd try to
+// get an object of that type from GetIt.
 
-// REGISTERING SINGLETONS:
+// *REGISTERING SINGLETONS:
 // GetIt gives us two options when it comes to singletons. We can either
 // 'registerSingleton' or 'registerLazySingleton'. The only difference between
 // them is that a non-lazy singleton is always registered immediately after the
 // app starts, while a lazy singleton is registered only when it's requested as
 // a dependency for some other class.
 
-// REGISTERING A REPOSITORY:
+// *REGISTERING A REPOSITORY:
 // While InputConverter is a stand-alone class, both of the use cases require a
 // NumberTriviaRepository. Notice that they depend on the ​contract​​ and not on
 // the concrete implementation. However, we cannot instantiate a ​​​contract (which
@@ -119,7 +125,7 @@ Future<void> init() async {
 // swapping the NumberTriviaRepository's underlying implementation for a
 // different one without any changes to the dependent classes.
 
-// REGISTERING EXTERNAL DEPENDENCIES:
+// *REGISTERING EXTERNAL DEPENDENCIES:
 // We've moved all the way down the call chain into the realm of 3rd party
 // libraries. We need to register a http.Client, DataConnectionChecker and also
 // SharedPreferences. The last one is a little tricky.
